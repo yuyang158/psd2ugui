c3cfc0564a98ef07d8843f462927a0fc2241846e
yuyang158 <yuyang_158@hotmail.com>
2020-12-23 16:35:55 +0800

    默认处理贴图格式

---
+++ Assets/Baum2/Editor/Scripts/BaumSpriteProcess.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Baum2.Editor {
+	public class BaumSpriteProcess : AssetPostprocessor {
+		private void OnPostprocessTexture(Texture2D texture) {
+			if( !assetPath.Contains("Assets/Art/UI/Sprites") ) {
+				return;
+			}
+			var importer = assetImporter as TextureImporter;
+			importer.textureType = TextureImporterType.Sprite;
+			importer.mipmapEnabled = false;
+		}
+	}
+}
